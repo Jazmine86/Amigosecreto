a53a28aae650c47dd4ae96133d0a2e575a0987a2
Jazmine <156954712+Jazmine86@users.noreply.github.com>
2025-03-17 01:19:38 -0600

    Creando el proyecto

---
+++ app.js
@@ -0,0 +1,24 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const inputAmigo=document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos=document.getElementById("listaAmigos");
+const ulResultado=document.getElementById("resultado");
+function agregarAmigo(){
+   if (!inputAmigo.value){
+        alert("Debes colocar el nombre de un amigo");
+    }
+    listaAmigos.push(inputAmigo.value);
+   // console.log(listaAmigos);
+   ulListaAmigos.innerHTML +=`<li>${inputAmigo.value}</li>`
+   document.getElementById("amigo").value="";
+
+   
+}
+
+function sortearAmigo() {
+    const random = Math.floor(Math.random()* listaAmigos.length);
+    const amigoSecreto= listaAmigos[random];
+    ulResultado.innerHTML=`<li>El Amigo Secreto sorteado es: ${amigoSecreto}</li>`;
+
+}
